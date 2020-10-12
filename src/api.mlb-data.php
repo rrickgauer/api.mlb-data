32c8e672b9714b0bf92d688b8d08782fdce41971
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-10-12 16:57:14 -0500

    url parsing

--- src/api.mlb-data.php
+++ src/api.mlb-data.php
@@ -11,7 +11,16 @@ $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 
 
 $module = $request[0];
-$playerID = $request[1];
+
+// ensure that a player id was provided in the url
+if (!isset($request[1])) {
+  http_response_code(404);
+  echo 'Please provide a player ID';
+  exit;
+} else {
+  $playerID = $request[1];
+}
+
 
 
 // check if player exists
