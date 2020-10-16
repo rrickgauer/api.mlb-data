4d32f2df38e96812ce31e7748d6b83220e356d8b
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-10-16 10:18:48 -0500

    add batting constants

---
+++ src/constants.php
@@ -0,0 +1,91 @@
+<?php
+
+
+
+class Constants {
+
+  const Batting = [
+    "ID"        => "ID",
+    "playerID"  => "playerID",
+    "yearID"    => "yearID",
+    "stint"     => "stint",
+    "teamID"    => "teamID",
+    "team_ID"   => "team_ID",
+    "lgID"      => "lgID",
+    "G"         => "G",
+    "G_batting" => "G_batting",
+    "AB"        => "AB",
+    "R"         => "R",
+    "H"         => "H",
+    "2B"        => "2B",
+    "3B"        => "3B",
+    "HR"        => "HR",
+    "RBI"       => "RBI",
+    "SB"        => "SB",
+    "CS"        => "CS",
+    "BB"        => "BB",
+    "SO"        => "SO",
+    "IBB"       => "IBB",
+    "HBP"       => "HBP",
+    "SH"        => "SH",
+    "SF"        => "SF",
+    "GIDP"      => "GIDP",
+  ];
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
