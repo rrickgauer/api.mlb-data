6ebddbb62503e8483607478b6b58d331ade9d407
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-10-19 22:13:38 -0500

    add default switch

--- src/main.php
+++ src/main.php
@@ -4,22 +4,20 @@ include_once('includes.php');
 include_once('Modules.php'); // filters, sorts, sortType, perPage, page
 include_once('Parser.php');
 
-
-// if (!$_SERVER['PATH_INFO'])
-//   throw new Exception('No path set');
-// else
-//   echo $_SERVER['PATH_INFO'];
-
-
 $p1 = new Parser();
-
 $sort = $p1->getSorts();
 
-$pitch = new Pitching($p1->getFilters(), $p1->getSorts(), 100, 0);
 
-$pitch->returnData();
+switch ($p1->getModule()) {
+  case Constants::Modules['Pitching']:
+    $pitch = new Pitching($p1->getFilters(), $p1->getSorts(), 100, 0);
+    $pitch->returnData();
+    break;
 
-
+  default:
+    ApiFunctions::returnDefaultDisplay();
+    break;
+}
 
 
 exit;
