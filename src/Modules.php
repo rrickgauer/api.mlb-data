2d0c5c440a734e004ed844d4f38a2e763194ca0d
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-10-19 20:32:06 -0500

    reindent lines

--- src/Modules.php
+++ src/Modules.php
@@ -2,8 +2,7 @@
 
 include_once('constants.php');
 
-class Module 
-{
+class Module {
 
     protected $filters;     // array of filters
     protected $sorts;       // array of sorts
@@ -13,78 +12,77 @@ class Module
     protected $dataSet;
 
     public function __construct($newFilters, $newSorts, $newSortType, $newPerPage, $newPage) {
-      $this->setFilters($newFilters);
-      $this->setSorts($newSorts);
-      $this->setSortType($newSortType);
-      $this->setPerPage($newPerPage);
-      $this->setPage($newPage);
-      $this->dataSet = null;
+        $this->setFilters($newFilters);
+        $this->setSorts($newSorts);
+        $this->setSortType($newSortType);
+        $this->setPerPage($newPerPage);
+        $this->setPage($newPage);
+        $this->dataSet = null;
     }
 
 
     public function getFilters() {
-      return $this->filters;
+        return $this->filters;
     }
 
     public function setFilters($newFilters) {
-      $this->filters = $newFilters;
+        $this->filters = $newFilters;
     }
 
     public function getSorts() {
-      return $this->sorts;
+        return $this->sorts;
     }
 
     public function setSorts($newSorts) {
-      $this->sorts = $newSorts;
+        $this->sorts = $newSorts;
     }
 
     public function getSortType() {
-      return $this->sortType;
+        return $this->sortType;
     }
 
     public function setSortType($newSortType) {
-      $newSortType = strtoupper($newSortType);
+        $newSortType = strtoupper($newSortType);
 
         // ensure that the sort type is either ASC or DESC
-      if ($newSortType == 'ASC')
-        $this->sortType = $newSortType;
-      else
-       $this->sortType = 'DESC'; 
-   }
+        if ($newSortType == 'ASC')
+            $this->sortType = $newSortType;
+        else
+            $this->sortType = 'DESC'; 
+    }
 
-   public function getPerPage() {
-    return $this->perPage;
-  }
+    public function getPerPage() {
+        return $this->perPage;
+    }
 
-  public function setPerPage($newPerPage) {
-    if ($newPerPage < 0)
-      $newPerPage = Constants::Defaults['PerPage'];
-    else if ($newPerPage > Constants::Limits['PerPage'])
-      $newPerPage = Constants::Limits['PerPage'];
-    else
-      $this->perPage = $newPerPage;
-  }
+    public function setPerPage($newPerPage) {
+        if ($newPerPage < 0)
+            $newPerPage = Constants::Defaults['PerPage'];
+        else if ($newPerPage > Constants::Limits['PerPage'])
+            $newPerPage = Constants::Limits['PerPage'];
+        else
+            $this->perPage = $newPerPage;
+    }
 
-  public function getPage() {
-    return $this->page;
-  }
+    public function getPage() {
+        return $this->page;
+    }
 
-  public function setPage($newPage) {
+    public function setPage($newPage) {
+        if ($newPage < 0)
+            $newPage = Constants::Defaults['Page'];
+        else
+            $this->page = $newPage;
+    }
 
-    if ($newPage < 0)
-      $newPage = Constants::Defaults['Page'];
-    else
-      $this->page = $newPage;
-  }
-
-  public function returnData() {
-    ApiFunctions::printJson($this->dataSet);
-  }
+    public function returnData() {
+        ApiFunctions::printJson($this->dataSet);
+    }
 }
 
 
-class Pitching extends Module
-{
+class Pitching extends Module {
+
     public function __construct($newFilters, $newSorts, $newSortType, $newPerPage, $newPage) {
         parent::__construct($newFilters, $newSorts, $newSortType, $newPerPage, $newPage);
         $this->retrieveData();
@@ -95,8 +93,7 @@ class Pitching extends Module
     }
 }
 
-class Batting extends Module
-{
+class Batting extends Module {
     public function __construct($newFilters, $newSorts, $newPerPage = Constants::Defaults['PerPage'], $newPage = Constants::Defaults['Page']) {
         parent::__construct($newFilters, $newSorts, $newPerPage = Constants::Defaults['PerPage'], $newPage = Constants::Defaults['Page']);
         $this->retrieveData();
