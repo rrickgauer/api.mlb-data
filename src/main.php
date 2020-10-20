2a796291e8574a4549c6c59a47733db9f805f859
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-10-20 11:53:10 -0500

    make main have 1 result set

--- src/main.php
+++ src/main.php
@@ -4,56 +4,42 @@ include_once('includes.php');
 include_once('Modules.php'); // filters, sorts, sortType, perPage, page
 include_once('Parser.php');
 
+// display default return if no module is specified
+if (!isset($_SERVER['PATH_INFO'])) {
+  ApiFunctions::returnDefaultDisplay();
+  exit;
+}
+
 $p1 = new Parser();
 $sort = $p1->getSorts();
 
-
+// generate the results based on the specified module
 switch ($p1->getModule()) {
   case Constants::Modules['Pitching']:
-    $pitch = new Pitching($p1->getFilters(), $p1->getSorts(), 100, 0);
-    $pitch->returnData();
-    break;
-
+    $results = new Pitching($p1->getFilters(), $p1->getSorts(), 100, 0); break;
   case Constants::Modules['Fielding']:
-    $fielding = new Fielding($p1->getFilters(), $p1->getSorts(), 100, 0);
-    $fielding->returnData();
-    break;
-
+    $results = new Fielding($p1->getFilters(), $p1->getSorts(), 100, 0); break;
   case Constants::Modules['Appearances']:
-    $appearances = new Appearances($p1->getFilters(), $p1->getSorts(), 100, 0);
-    $appearances->returnData();
-    break;
-
+    $results = new Appearances($p1->getFilters(), $p1->getSorts(), 100, 0); break;
   case Constants::Modules['FieldingOF']:
-    $fieldingOF = new FieldingOF($p1->getFilters(), $p1->getSorts(), 100, 0);
-    $fieldingOF->returnData();
-    break;
-
+    $results = new FieldingOF($p1->getFilters(), $p1->getSorts(), 100, 0); break;
   case Constants::Modules['FieldingOFSplit']:
-    $fieldingOFSplit = new FieldingOFSplit($p1->getFilters(), $p1->getSorts(), 100, 0);
-    $fieldingOFSplit->returnData();
+    $results = new FieldingOFSplit($p1->getFilters(), $p1->getSorts(), 100, 0);
     break;
-
   case Constants::Modules['Salaries']:
-    $salaries = new Salaries($p1->getFilters(), $p1->getSorts(), 100, 0);
-    $salaries->returnData();
-    break;
-
+    $results = new Salaries($p1->getFilters(), $p1->getSorts(), 100, 0); break;
   case Constants::Modules['People']:
-    $people = new People($p1->getFilters(), $p1->getSorts(), 100, 0);
-    $people->returnData();
-    break;
-
+    $results = new People($p1->getFilters(), $p1->getSorts(), 100, 0);break;
   default:
-    ApiFunctions::returnDefaultDisplay();
+    ApiFunctions::returnBadRequest('Module does not exist!');
+    exit;
     break;
 }
 
+// return the results
+$results->returnData();
 
 exit;
-
-
-
 
 
 ?>
