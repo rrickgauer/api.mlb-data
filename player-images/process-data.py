a349f725164025f3b8c84f8327877e9fda14b368
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-10-30 11:54:08 -0500

    create insert statements

--- player-images/process-data.py
+++ player-images/process-data.py
@@ -6,9 +6,29 @@ def getData(fileName):
       return configData
 
 
+def getInsertStmt(playerID, source):
+  # print(playerID)
+
+  stmt = 'INSERT INTO images (playerID, source) VALUES ("'
+  stmt += playerID + '", "'
+  stmt += source + '");'
+  return stmt
+
+
 FILE_NAME = 'player-images.json'
 
 
 data = getData(FILE_NAME)
 
 print(len(data))
+
+
+outputFile = open('insert-images.sql', 'w')
+
+for row in data:
+  playerID = row["playerID"]
+  source = row["imgUrl"]
+  stmt = getInsertStmt(playerID, source)
+  outputFile.write(stmt + "\n")
+
+outputFile.close()
