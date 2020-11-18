7d1e67c3015d388bf3f8beb221589dffb672fc76
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-11-17 20:15:57 -0600

    create filter and sort classes

--- src/Common-Structures.php
+++ src/Common-Structures.php
@@ -1,6 +1,52 @@
 <?php
 
 
+class Filter {
+  private $column;
+  private $conditional;
+  private $qualifier;
+
+  public function __construct($column, $conditional, $qualifier) {
+    $this->column = $column;
+    $this->conditional = $conditional;
+    $this->qualifier = $qualifier;
+  }
+
+  public function getColumn() {
+    return $this->column;
+  }
+
+  public function getConditional() {
+    return $this->conditional;
+  }
+
+  public function getQualifier() {
+    return $this->qualifier;
+  }
+}
+
+
+class Sort {
+  private $column;
+  private $type;
+
+  public function __construct($column, $type) {
+    $this->column = $column;
+    $this->type = $type;
+  }
+
+  public function getColumn() {
+    return $this->column;
+  }
+
+  public function getType() {
+    return $this->type;
+  }
+}
+
+
+
+
 
 
 
